42e80eed06c306c89e6ca3be6fbc1fd96f080e5c
Viet <gh.hrnq1@gmail.com>
2022-09-02 22:12:51 -0300

    Solução exercicio

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine(@"O professor:
+– Quem se acha burro fique em pé.
+Joãozinho se levanta:
+– Você se acha burro, Joãozinho?
+– Não, mas fiquei com dó de ver o senhor em pé sozinho.");
+
